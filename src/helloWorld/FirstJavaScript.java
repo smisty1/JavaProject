c4308e0d70d3662afc8561407d245202fe79095d
FUAD HOSSAIN <FUAD HOSSAIN@DESKTOP-CTFSMH2.lan>
2022-03-11 00:20:24 -0500

    1st commit

---
+++ src/helloWorld/FirstJavaScript.java
@@ -0,0 +1,23 @@
+package helloWorld;
+
+public class FirstJavaScript {
+
+	public static void main(String[] args) {
+		
+ int x =20;
+ String line = "i am learning java";
+
+ System.out.println(x);
+ System.out.println(line);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+	}
+
+}
